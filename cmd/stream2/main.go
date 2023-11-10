4433ebcc719071bdb0804e1a139e773c3e3a6af5
root <root@LAPTOP-B8OJ4365>
2023-11-10 16:01:54 +0700

    test

--- cmd/stream2/main.go
+++ cmd/stream2/main.go
@@ -1,32 +1,31 @@
 package iot
 
-import (
-	chclient "iot-stream/internal/client"
-	"iot-stream/share/cos"
-	"log"
-	"os"
-)
-func Client(config *chclient.Config) {
-	config.Server = "https://tunnel.rogo.com.vn"
-	config.Remotes = []string{"R:localhost:5555"}
-	//default auth
-	if config.Auth == "" {
-		config.Auth = os.Getenv("AUTH")
-	}
+import "fmt"
 
-	//ready
-	c, err := chclient.NewClient(config)
+// chclient "iot-stream/internal/client"
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	c.Debug = true
-	go cos.GoStats()
-	ctx := cos.InterruptContext()
-	if err := c.Start(ctx); err != nil {
-		log.Fatal(err)
-	}
-	if err := c.Wait(); err != nil {
-		log.Fatal(err)
-	}
+func Client() {
+	fmt.Print("ssssssssssssssss")
+	// config.Server = "https://tunnel.rogo.com.vn"
+	// config.Remotes = []string{"R:localhost:5555"}
+	// //default auth
+	// if config.Auth == "" {
+	// 	config.Auth = os.Getenv("AUTH")
+	// }
+
+	// //ready
+	// c, err := chclient.NewClient(config)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// c.Debug = true
+	// go cos.GoStats()
+	// ctx := cos.InterruptContext()
+	// if err := c.Start(ctx); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// if err := c.Wait(); err != nil {
+	// 	log.Fatal(err)
+	// }
 }
