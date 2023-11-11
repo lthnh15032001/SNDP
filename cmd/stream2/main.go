e0b908418909c35dbce029ca2edb80690bbe9656
lthnh15032001 <50706080+lthnh15032001@users.noreply.github.com>
2023-11-12 02:11:17 +0700

    mtest

--- cmd/stream2/main.go
+++ cmd/stream2/main.go
@@ -5,10 +5,10 @@ import (
 	"os"
 
 	chclient "github.com/lthnh15032001/ngrok-impl/internal/client"
-	"github.com/lthnh15032001/ngrok-impl/share/cos"
 )
 
-func Client(config chclient.Config) bool {
+func Client() bool {
+	config := chclient.Config{}
 	config.Server = "https://tunnel.rogo.com.vn"
 	config.Remotes = []string{"R:localhost:5555"}
 	//default auth
@@ -22,15 +22,15 @@ func Client(config chclient.Config) bool {
 	if err != nil {
 		log.Fatal(err)
 	}
-	c.Debug = true
-	go cos.GoStats()
-	ctx := cos.InterruptContext()
-	if err := c.Start(ctx); err != nil {
-		log.Fatal(err)
-	}
-	if err := c.Wait(); err != nil {
-		log.Fatal(err)
-	}
+	// c.Debug = true
+	// go cos.GoStats()
+	// ctx := cos.InterruptContext()
+	// if err := c.Start(ctx); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// if err := c.Wait(); err != nil {
+	// 	log.Fatal(err)
+	// }
 	return true
 }
 
